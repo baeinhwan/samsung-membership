52089aeca2e03c1a77c8cade113687f2e74c2586
baeinhwan <in-hwan-good@hanmail.net>
2012-07-20 08:02:40 +0900

    inhwan3

---
+++ systemProcessModuleCshap/systemProcessModule/Hpacket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace systemProcessModule
+{
+    [Serializable]
+    class Hpacket
+    {
+        public byte[] ID = new byte[4];
+        public byte[] Length = new byte[4];
+
+        public const byte EmergencyStop = 0x11;
+        public const byte OPERATION_MODE = 0x21;
+        public const byte DRIVING_MODE = 0x31;
+        public const byte CRUISE_CONTROL = 0x46;
+
+
+
+    }
+}
